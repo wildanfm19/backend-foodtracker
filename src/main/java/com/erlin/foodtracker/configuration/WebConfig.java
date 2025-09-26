e94766707c0798cd26cf81d4815ed7c9d1f18535
wildanfm19 <dracko.maaruftm@gmail.com>
2025-09-26 22:11:37 +0700

    Testing for vercel

--- src/main/java/com/erlin/foodtracker/configuration/WebConfig.java
+++ src/main/java/com/erlin/foodtracker/configuration/WebConfig.java
@@ -10,10 +10,7 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(
-                        "http://localhost:4200",
-                        "https://dailybytes-blue.vercel.app"
-                                )
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE" , "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*");
