549cfda72a89b2539dd9ca5c8e4555939fc9550c
wildanfm19 <dracko.maaruftm@gmail.com>
2025-09-26 22:03:20 +0700

    Added CORS for vercel

--- src/main/java/com/erlin/foodtracker/configuration/WebConfig.java
+++ src/main/java/com/erlin/foodtracker/configuration/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://dailybytes-blue.vercel.app"
+                                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE" , "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*");
